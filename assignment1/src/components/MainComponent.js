940807af90a077078e668f0a39819d3611fb1326
Huu Thom <thomnhfx12107@funix.edu.vn>
2022-01-17 11:09:38 +0700

    Create react Assignment 1

---
+++ assignment1/src/components/MainComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>App</h1>
+                <p>Start create React app</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Main
