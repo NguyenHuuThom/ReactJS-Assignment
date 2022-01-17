37f9146ef19e7baa62f3de67277619ddfd6eb60b
Huu Thom <thomnhfx12107@funix.edu.vn>
2022-01-17 15:34:58 +0700

    StaffList revert

--- assignment1/src/components/StaffListComponent.js
+++ assignment1/src/components/StaffListComponent.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Card, CardTitle, } from 'reactstrap';
 
+function RenderStaffList({ staff }) {
+    return (
+        <Card className="btn-linkedin">
+            <CardTitle className="text-center m-1 text-white" heading="true">{staff.name}</CardTitle>
+        </Card>
+    )
+}
+
 const StaffList = (props) => {
     const staff = props.staffs.map((staff) => {
         return (
             <div key={staff.id} className="col-12 col-sm-6 col-md-4 col-xl-2 mt-4">
-                <Card>
-                    <CardTitle className="text-center m-1" heading="true">{staff.name}</CardTitle>
-                </Card>
+                <RenderStaffList staff={staff} onClick={props.onClick} />
             </div>
         );
     });
-
     return (
         <div className="container">
             <div className="row">
