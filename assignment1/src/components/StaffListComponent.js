9752e5ec744d6325d3f741b6bb97fb36fbb25a1a
Huu Thom <thomnhfx12107@funix.edu.vn>
2022-01-18 12:53:38 +0700

    assignment 1 update bg-color

--- assignment1/src/components/StaffListComponent.js
+++ assignment1/src/components/StaffListComponent.js
@@ -21,21 +21,23 @@ class StaffList extends Component {
             const startDate = dateFormat(staff.startDate, "dd/mm/yyyy")
 
             return (
-                <Card className="col-12 col-md-12 col-lg-6 m-3">
-                    <div className="d-flex">
-                        <div className="m-2 mt-3">
-                            <img src={staff.image} alt={staff.name} />
+                <div className="col">
+                    <Card className="col-12 col-md-12 col-lg-6 mt-3 bg-primary">
+                        <div className="d-flex text-white">
+                            <div className="m-2 mt-3">
+                                <img src={staff.image} alt={staff.name} />
+                            </div>
+                            <div className="m-2">
+                                <h5><b>{`Họ và tên: ${staff.name}`}</b></h5>
+                                <p>{`Ngày sinh: ${dateOfBirth}`}</p>
+                                <p>{`Ngày vào công ty: ${startDate}`}</p>
+                                <p>{`Phòng ban: ${staff.department.name}`}</p>
+                                <p>{`Ngày nghỉ còn lại: ${staff.annualLeave}`}</p>
+                                <p>{`Số ngày đã làm thêm : ${staff.overTime}`}</p>
+                            </div>
                         </div>
-                        <div className="m-2">
-                            <h5><b>{`Họ và tên: ${staff.name}`}</b></h5>
-                            <p>{`Ngày sinh: ${dateOfBirth}`}</p>
-                            <p>{`Ngày vào công ty: ${startDate}`}</p>
-                            <p>{`Phòng ban: ${staff.department.name}`}</p>
-                            <p>{`Ngày nghỉ còn lại: ${staff.annualLeave}`}</p>
-                            <p>{`Số ngày đã làm thêm : ${staff.overTime}`}</p>
-                        </div>
-                    </div>
-                </Card>
+                    </Card>
+                </div>
             )
         }
         else {
@@ -49,9 +51,9 @@ class StaffList extends Component {
         const staff = this.props.staffs.map((staff) => {
             return (
                 <div key={staff.id} className="col-12 col-sm-6 col-md-4 col-xl-2 mt-4">
-                    <Card onClick={() => this.onStaffSelect(staff)}>
+                    <Card onClick={() => this.onStaffSelect(staff)} className="bg-primary">
                         <CardImg width="100%" src={staff.image} alt={staff.name} />
-                        <CardTitle className="text-center m-1" heading="true">{staff.name}</CardTitle>
+                        <CardTitle className="text-center m-1 text-white" heading="true">{staff.name}</CardTitle>
                     </Card>
                 </div>
             );
