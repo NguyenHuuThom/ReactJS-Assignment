5cd8778c6c5202d3d98ff49d02fe192725382be4
Huu Thom <thomnhfx12107@funix.edu.vn>
2022-01-21 20:28:21 +0700

    from asm3

---
+++ assignment3/src/component/SalaryComponent.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+function RenderStaffSalary({ staff }) {
+    const basicSalary = 3000000;
+    const overTimeSalary = 200000;
+    const salary = (staff.salaryScale * basicSalary) + (staff.overTime * overTimeSalary);
+
+    return (
+        <Card className="bg-info text-white">
+            <CardTitle className="ml-2 m-1 mt-2" heading="true"><b>{staff.name}</b></CardTitle>
+            <CardText className="ml-2 m-1">{`Mã nhân viên: ${staff.id}`}</CardText>
+            <CardText className="ml-2 m-1">{`Hệ số lương: ${staff.salaryScale}`}</CardText>
+            <CardText className="ml-2 m-1">{`Số giờ làm thêm: ${staff.overTime}`}</CardText>
+            <CardText className="ml-2 m-1">{`Lương: ${salary}`}</CardText>
+        </Card>
+    )
+}
+
+function Salary(props) {
+    const staffs = (props.staffs).map((staff) => {
+        return (
+            <div key={staff.id} className="col-12 col-sm-6 col-md-6 col-lg-4 mt-4">
+                <RenderStaffSalary staff={staff} />
+            </div>
+        );
+    });
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/staff">Nhân Viên</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
+                </Breadcrumb>
+            </div>
+            <div className="row">
+                {staffs}
+            </div>
+        </div>
+    );
+}
+
+export default Salary;
---
+++ assignment3/src/component/StaffInfoComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Card, CardGroup, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import dateFormat from "dateformat";
+
+function RenderStaff(staff) {
+    const dateOfBirth = dateFormat(staff.staff.doB, "dd/mm/yyyy")
+    const startDate = dateFormat(staff.staff.startDate, "dd/mm/yyyy")
+    return (
+        <CardGroup>
+            <Card className="bg-info p-2 col-sm-4 col-md-3 col-lg-4 m-0" >
+                <CardImg src={staff.staff.image} alt={staff.staff.name} />
+            </Card>
+            <Card className="bg-info text-white p-2">
+                <CardTitle><b>{`Họ và tên: ${staff.staff.name}`}</b></CardTitle>
+                <CardText>{`Ngày sinh: ${dateOfBirth}`}</CardText>
+                <CardText>{`Ngày vào công ty: ${startDate}`}</CardText>
+                <CardText>{`Phòng ban: ${staff.staff.department.name||staff.staff.department}`}</CardText>
+                <CardText>{`Ngày nghỉ còn lại: ${staff.staff.annualLeave}`}</CardText>
+                <CardText>{`Số giờ đã làm thêm : ${staff.staff.overTime}`}</CardText>
+            </Card>
+        </CardGroup>
+    );
+}
+
+const StaffInfo = (props) => {
+    if (props.staff != null)
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/staff">Nhân Viên</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.staff.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.staff.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-6 m-1">
+                        <RenderStaff staff={props.staff} />
+                    </div>
+                </div>
+            </div>
+        );
+    else
+        return (
+            <div></div>
+        );
+}
+
+export default StaffInfo;
